801d5766030b1141bb8f2e22e227741ef563aca4
sbkhaloof <sehammalkawi92@gmail.com>
2021-06-28 16:49:05 +0300

    lab2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './component /Header';
 import Main from './component /Main';
 import Footer from './component /Footer';
+
 class App extends React.Component{
   render(){
     return (

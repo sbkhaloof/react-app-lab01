a37509567b1c402e326b6c2da870d50478fb1dc6
sbkhaloof <sehammalkawi92@gmail.com>
2021-06-29 23:10:57 +0300

    lab3

--- src/component /HornedBeasts.js
+++ src/component /HornedBeasts.js
@@ -1,50 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from  'react-bootstrap/Card';
-class HornedBeasts extends React.Component 
-{
-    constructor(props) 
-    {
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+
+class HornedBeasts extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = 
+        this.state =
         {
             numberOfPets: 0
         }
     }
 
-    increaseNoOfPets = () =>{
+    increaseNoOfPets = () => {
         this.setState({
-            numberOfPets: this.state.numberOfPets +1
+            numberOfPets: this.state.numberOfPets + 1
         })
+
     }
+    
+    render() {
+        return (
+            <>
+                <Card onClick={this.props.findItem} style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.image_url} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            {this.props.description}
+                        </Card.Text>
+                        <Button variant="primary" onClick={this.increaseNoOfPets}>👍{this.state.numberOfPets}</Button>
+                    </Card.Body>
+                </Card>
 
-
-render() 
-{
-    return (
-        <>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={this.props.image_url} />
-  <Card.Body>
-    <Card.Title>{this.props.title}</Card.Title>
-    <Card.Text>
-    {this.props.description}
-    </Card.Text>
-    <Card.Text onClick={this.increaseNoOfPets}>👍{this.state.numberOfPets}
-</Card.Text>
-  </Card.Body>
-</Card>
-
-            {/* <h2> {this.props.title}</h2>
+                {/* <h2> {this.props.title}</h2>
             <img src={this.props.image_url} alt={this.props.title} width="300" />
             <p>
                 {this.props.description}
             </p >
             <p>👍{this.state.numOfPets}</p> */}
 
-        </>
-    )
-}
+            </>
+        )
+    }
 }
 
 export default HornedBeasts;
--- src/component /Main.js
+++ src/component /Main.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import HornedB from './HornedB.json';
+import SelectedBeast from './SelectedBeast';
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            showPea: false,
+            hornedObj:{}
+        }
+    }
+       
+    findItem = (title) => {
+       let result= HornedB.find(element =>{ 
+      if(element.title===title)
+      {return element;}
+       }) 
+    this.setState({
+        showPea:true,
+        hornedObj:result
+    })
+    endShow = ()=>{
+        this.setState({
+            showPea:false,
+            hornedObj:{}
+        })
+    }
+}
+
     render() {
         return (
             <div className="main">
                 {HornedB.map((item, index) => {
-                    return <HornedBeasts title={item.title} description={item.description} image_url={item.image_url} key={index} index={index} />
+                    return (<HornedBeasts findItem={this.findItem}   title={item.title} description={item.description} 
+                        image_url={item.image_url} key={index} index={index} />)
                 })}
-                     </div>
+                <SelectedBeast findIteme={this.findItem} clickHornedBeasts={this.state.result} endShow={this.endShow}/>
+            </div>
         );
     }
 }

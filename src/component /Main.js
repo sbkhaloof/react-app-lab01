4c9a15aea5447a935e0d3ede03e4a9bff9049492
sbkhaloof <sehammalkawi92@gmail.com>
2021-06-30 16:02:06 +0300

    solve lab3

--- src/component /Main.js
+++ src/component /Main.js
@@ -2,26 +2,25 @@ import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import HornedB from './HornedB.json';
 import SelectedBeast from './SelectedBeast';
-class Main extends React.Component
- {
-  
-   
-        render() {
-            return (
-                <div className="main">
-                    {this.props.dataArray.map((item, index) => {
-                        // return (<HornedBeasts findItem={this.findItem} title={item.title} description={item.description}
-                        //     image_url={item.image_url} key={index} index={index} />)
-                    return(
-                    <HornedBeasts 
-                    key={index} title={item.title} description={item.description}
-                     image_url={item.image_url} findFunc={this.props.findItem} />)
-                    })}
-                    { console.log(this.props.dataArray)}
-                </div>
-               
-            );
-        }
-    }
+class Main extends React.Component {
 
-    export default Main;
+
+    render() {
+        return (
+            <div className="main">
+                {this.props.dataArray.map((item, index) => {
+                    // return (<HornedBeasts findItem={this.findItem} title={item.title} description={item.description}
+                    //     image_url={item.image_url} key={index} index={index} />)
+                    return (
+                        <HornedBeasts
+                            key={index} title={item.title} description={item.description}
+                            image_url={item.image_url} findFunc={this.props.findItem} />)
+                })}
+               
+            </div>
+
+        );
+    }
+}
+
+export default Main;
